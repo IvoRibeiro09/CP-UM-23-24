780e0792c8521087aa67bac2c10d6136ba34e942
IvoRibeiro09 <ivomiguelalvesribeiro@gmail.com>
2023-11-15 11:28:35 +0000

    update start tp2

---
+++ TP1/test/code/src/function.h
@@ -0,0 +1,17 @@
+#include "function.cpp"
+
+void initialize();
+
+void MeanSquaredVelocity_and_Kinetic();
+
+void computeAccelerations_plus_potential();
+
+void clearAmatrix();
+
+double VelocityVerlet(double dt, int iter, FILE *fp);
+
+void initializeVelocities();
+
+double gaussdist();
+
+
